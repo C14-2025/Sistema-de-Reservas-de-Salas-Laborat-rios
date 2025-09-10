aa1c6ad8b8028d346a511a385b1698dbf4c62de1
Romulo Beluco <148589630+romulobeluco@users.noreply.github.com>
2025-09-10 11:48:21 -0300

    feat(auth): conexão do banco de dados com email e senha. Criação de criptografia para a senha

---
+++ backend/db.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+import os
+
+MONGO_URI = os.getenv(
+    "MONGO_URI",
+    
+    "mongodb+srv://rm:7OVHYULulWKZuBfD@cluster0.qqpv0z8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+)
+DB_NAME = os.getenv("db", "users")
+
+try:
+    client = MongoClient(MONGO_URI)
+    # teste de conexão
+    client.admin.command("ping")
+    print("✅ Conexão com MongoDB Atlas bem-sucedida!")
+except Exception as e:
+    print("❌ Erro ao conectar no MongoDB:", e)
+
+db = client[DB_NAME]
+users_coll = db["users"]
